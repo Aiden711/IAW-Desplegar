fffc9773ab7b8dc7ac529be95a50899fe5869f70
Aiden711 <ivan.g.m.711@gmail.com>
2025-01-07 18:11:40 +0100

    v1.2

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -10,6 +10,5 @@ DEBUG = True # En despliegue esto pasa a FALSE
 # Ejemplo basico de postgresql
 # Comparalo con ejemplo de Mysql
 #postgresql:
-SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://ivan:0qf6sPLSbBxFphNjo3oJ6Z6gZJOh5n8y@dpg-ctuh4sl2ng1s739e5mgg-a.oregon-postgres.render.com/dbase_0wpt'
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://ivan:G7RWX0YvaUrIl8VPQaLlMR0J6xyWS72s@dpg-ctulv0i3esus739e41c0-a.oregon-postgres.render.com/dbase_3k8p'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
-
