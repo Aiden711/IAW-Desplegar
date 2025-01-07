6574a090a74a6c7d1271485c6627e46af27fc9db
Aiden711 <ivan.g.m.711@gmail.com>
2025-01-07 18:06:19 +0100

    v1.1

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -4,11 +4,12 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True # En despliegue esto pasa a FALSE
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Ejemplo basico de postgresql
 # Comparalo con ejemplo de Mysql
-#SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:password@localhost/base_datos'
-#SQLALCHEMY_TRACK_MODIFICATIONS=False
+#postgresql:
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://ivan:0qf6sPLSbBxFphNjo3oJ6Z6gZJOh5n8y@dpg-ctuh4sl2ng1s739e5mgg-a.oregon-postgres.render.com/dbase_0wpt'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
 
